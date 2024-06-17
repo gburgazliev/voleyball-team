c9faf2dc7f2dbc3de8f4817fea1bafe7b30b8110
Georgi Burgazliev <nanobat6@gmail.com>
2024-06-17 12:57:49 +0300

    add secure firebase-config

--- my-app/firebase/firebase-config.js
+++ my-app/firebase/firebase-config.js
@@ -1,20 +1,22 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 import { getAnalytics} from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCvPMbe4TW9fEYcLYw3O42yTF5zgOVjrDU",
-  authDomain: "voleyball-team.firebaseapp.com",
-  projectId: "voleyball-team",
-  storageBucket: "voleyball-team.appspot.com",
-  messagingSenderId: "350177838327",
-  appId: "1:350177838327:web:2f9119281d1dd7f4fc3ff2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-525H0G45F7"
 };
 
@@ -25,3 +27,4 @@ const database = getDatabase(app);
 const auth = getAuth(app);
 
 export {database, app, auth, analytics}
+
--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -3,10 +3,13 @@ import { Box, Container, Heading } from "@chakra-ui/react"
 
 const Header = () => {
     return (
-     <Box >
-       <Box   textColor='orange' paddingLeft={50} paddingTop={10} maxW='15%' marginRight='5%'> 
+     <Box w='100%' h='30%'>
+        <Container >
+            <Box   textColor='orange' paddingTop={10}> 
            <Heading size='lg' _hover={{color:'blue', cursor: 'pointer'}} onClick={''}>  SB COMMUNITY</Heading>
          </Box>
+        </Container>
+       
         </Box>
     )
 }
