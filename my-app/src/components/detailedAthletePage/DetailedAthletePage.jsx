4e7b4f3f4bc0328ad3e5a67d35ad63dfc4fc072f
Georgi Burgazliev <nanobat6@gmail.com>
2024-06-27 17:33:24 +0300

    add admin functionality for editing athlete description

--- my-app/src/components/detailedAthletePage/DetailedAthletePage.jsx
+++ my-app/src/components/detailedAthletePage/DetailedAthletePage.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { getUserById } from "../../utils/utils";
-import { Box, Flex, Image, Text, Heading, List, ListItem, ListIcon, OrderedList, UnorderedList,Input, Button } from "@chakra-ui/react"
+import { Box, Flex, Image, Text, Heading, List, ListItem, ListIcon, OrderedList, UnorderedList, Input, Button, Container,Textarea } from "@chakra-ui/react"
 import { useAuth } from "../../context/AuthContext";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../../firebase/firebase-config";
-import { subscribeToAthleteById} from "../../utils/utils";
+import { subscribeToAthleteById } from "../../utils/utils";
 import { updateAthlete } from "../../utils/utils";
 
 
@@ -18,8 +18,9 @@ const DetailedAthletePage = () => {
     const [currUser, setUser] = useState({});
     const [userData, setUserData] = useState({});
     const [videoId, setVideoId] = useState('');
+    const [description, setDescription] = useState('');
 
-       
+
     useEffect(() => {
         const unsubscribe = subscribeToAthleteById(id.slice(1), setAthlete);
 
@@ -53,7 +54,7 @@ const DetailedAthletePage = () => {
         setVideoId('');
 
     }
-    
+
 
     const handleDeleteVideo = async () => {
         const videoProp = {
@@ -62,32 +63,48 @@ const DetailedAthletePage = () => {
         await updateAthlete(id.slice(1), videoProp);
         setVideoId('');
     }
-   
+
+    useEffect(() => {
+        setDescription(athlete?.description);
+    }, [athlete])
+
+    const handleSubmitDescription = async () => {
+        const descriptionProp = {
+            description: description
+        }
+        await updateAthlete(id.slice(1), descriptionProp);
+        setDescription(description);
+    }
+
+
     return (
         <Flex w='100%' h='100%' direction='column' justify='center' align='center'>
             <Flex w='50%' h='50%' direction='column' >
-                <iframe   width="100%"
-                        height="100%"
-                        src={`https://www.youtube.com/embed/${athlete?.videoID}`}
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Athlete Video">
+                <iframe width="100%"
+                    height="100%"
+                    src={`https://www.youtube.com/embed/${athlete?.videoID}`}
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Athlete Video">
                     Your browser does not support the video tag.
                 </iframe>
 
             </Flex>
 
-            { currUser.role === 'admin' && <Flex w='10%' h='10%' direction='column' justify='center' align='center'>
-                   
-               <Input type="text" value={videoId} onChange={(e) => setVideoId(e.target.value)}></Input>
-              {!athlete?.videoID && <Button colorScheme='red' onClick={handleAddVideo}>Add video</Button>}
-                {athlete?.videoID && <Button colorScheme='red'  onClick={handleDeleteVideo}>Delete video</Button>}
+            {currUser.role === 'admin' && <Flex w='10%' h='10%' direction='column' justify='center' align='center'>
+
+                <Input type="text" value={videoId} onChange={(e) => setVideoId(e.target.value)}></Input>
+                {!athlete?.videoID && <Button colorScheme='red' onClick={handleAddVideo}>Add video</Button>}
+                {athlete?.videoID && <Button colorScheme='red' onClick={handleDeleteVideo}>Delete video</Button>}
             </Flex>}
 
-            
-            <Flex w='50%' borderRadius='1px' bg='green'>
-                <Heading>{athlete?.firstname} {athlete?.lastname}</Heading>
+
+            <Flex w='50%' borderRadius='1px' bg='green' borderColor='green'>
+                {currUser.role !== 'admin' && <Container>{athlete?.description}</Container>}
+                {currUser.role === 'admin' && <Textarea value={description} onChange={(e) => setDescription(e.target.value)} ></Textarea>}
+
             </Flex>
+            {currUser.role === 'admin' && description.length > athlete?.description.length && <Button onClick={handleSubmitDescription}> Submit description</Button>}
         </Flex>
     )
 }
