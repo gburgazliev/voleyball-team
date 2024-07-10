e7bdcc0e8982c338f4bd0a0b8cc223c9d8157503
Georgi Burgazliev <nanobat6@gmail.com>
2024-07-10 20:54:17 +0300

    add google recaptcha to contact page

--- my-app/src/components/contactUsPage/ContactUsPage.jsx
+++ my-app/src/components/contactUsPage/ContactUsPage.jsx
@@ -1,18 +1,52 @@
-import {Flex, Box } from "@chakra-ui/react"
+import { Flex, Box, Input, Textarea, Button } from "@chakra-ui/react"
+import { auth } from "../../../firebase/firebase-config"
+import { useAuth } from "../../context/AuthContext";
+import { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import ReCAPTCHA from "react-google-recaptcha";
+import { useRef } from "react";
+import axios from "axios";
+
+
+
 
 const ContactUsPage = () => {
+    const [user, setUser] = useState(null);
+    const {captchaVal ,setCaptchaVal} = useState(null);
+    const [email, setEmail] = useState('');
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+
+ 
+
+    useEffect(() => {
+
+        onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        });
+
+    }, [])
+
+
+    const handleFormSubmit = async () => {
+
+     console.log(email, title, content, captchaVal);    
+
+    }
     return (
-        <Flex direction='column' h='100%' justify='center' align='center'>
-            <h1>Contact Us</h1>
-            <p>For more information, please contact us at:
-                <br />
-                <br />
-                Email:
-                <br />
-                Phone:
-                <br />
-                Address:
-            </p>
+        <Flex direction='column' w='100%' h='100%' justify='space-evenly' align='center'>
+            {!user && <Input w='50%' placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />}
+            <Input w='50%' placeholder="Title" />
+            <Textarea w='60%' h='50%' placeholder="Content" />
+            <ReCAPTCHA
+           alignSelf='flex-start'
+           
+            sitekey="6LcTDg0qAAAAAIBTo6i6tCEgpVdr6ZT9Rn0zDMEI"
+            size="normal"
+            onChange={(token) => { setCaptchaVal(token)}}
+        />
+            <Button disabled={!captchaVal} onClick={handleFormSubmit}>Submit</Button>
         </Flex>
     )
 }
