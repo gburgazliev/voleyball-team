e27bae8dcc7d8ba4bde34f25896e08e48145711f
Georgi Burgazliev <nanobat6@gmail.com>
2024-07-20 17:09:39 +0300

    change mobileheader buttons style

--- my-app/src/components/mobileHeader/MobileHeader.jsx
+++ my-app/src/components/mobileHeader/MobileHeader.jsx
@@ -16,6 +16,8 @@ import { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "../../../firebase/firebase-config"
 import { useAuth } from "../../context/AuthContext";
+import { color } from "framer-motion";
+import { useLocation } from 'react-router-dom';
 
 
 
@@ -25,6 +27,7 @@ const MobileHeader = () => {
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
     const { logout } = useAuth();
+    const location = useLocation();
 
 
     useEffect(() => {
@@ -49,14 +52,17 @@ const MobileHeader = () => {
                     <DrawerHeader borderBottomWidth='1px'>Menu</DrawerHeader>
                     <DrawerCloseButton />
                     <DrawerBody >
-                        <Text onClick={() => { navigate('/'); onClose() }} _hover={{ color: 'orange', cursor: 'pointer' }}>Home</Text>
-                        <Text>Coaches</Text>
-                        <Text onClick={() => { navigate('/about-us'); onClose() }}>About us</Text>
-                        <Text onClick={() => { navigate('/contact'); onClose() }} _hover={{ color: 'orange', cursor: 'pointer' }}>Contact us</Text>
-                        <Text>Media</Text>
+                        <Stack>
+                              <Button bg={location.pathname === '/' ? 'orange' : 'gray.100'}  _hover={{ color: 'orange', cursor: 'pointer' }} _active={{ backgroundColor: 'orange', color: 'white' }} onClick={() => { navigate('/') }}>Home</Button>
+                        <Button  _hover={{ color: 'orange', cursor: 'pointer' }} _active={{ backgroundColor: 'orange', color: 'white' }}>Coaches</Button>
+                        <Button bg={location.pathname === '/about-us' ? 'orange' : 'gray.100'}  _hover={{ color: 'orange', cursor: 'pointer' }}_active={{ backgroundColor: 'orange', color: 'white' }} onClick={() => { navigate('/about-us') }}>About us</Button>
+                        <Button  bg={location.pathname === '/contact' ? 'orange' : 'gray.100'}_hover={{ color: 'orange', cursor: 'pointer' }} _active={{ backgroundColor: 'orange', color: 'white' }} onClick={() => { navigate('/contact')}} >Contact us</Button>
+                        <Button>Media</Button>
+                        </Stack>
+                      
                     </DrawerBody>
                     <DrawerFooter >
-                        {user ? <Button onClick={() => { navigate('/'); logout(); onClose() }}>Sign Out</Button> : <> <Button m={2} onClick={() => navigate('/auth/login')}>Sign In</Button>
+                        {user ? <Button onClick={() => { navigate('/'); logout(); onClose()}}>Sign Out</Button> : <> <Button m={2} onClick={() => navigate('/auth/login')}>Sign In</Button>
                             <Button onClick={() => navigate('/auth/register')}>Sign Up</Button></>}
 
                     </DrawerFooter>
