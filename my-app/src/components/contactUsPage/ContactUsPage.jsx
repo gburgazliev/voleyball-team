5dfe2f0180b30bc48604b9a72111a45df1d9418a
Georgi Burgazliev <nanobat6@gmail.com>
2024-07-27 16:38:23 +0300

    reset fields on succesfully submitted contact msg

--- my-app/src/components/contactUsPage/ContactUsPage.jsx
+++ my-app/src/components/contactUsPage/ContactUsPage.jsx
@@ -113,6 +113,10 @@ const ContactUsPage = () => {
       );
     setIsLoading(false);
 
+    setName('');
+    setEmail('');
+    setContent('');
+
     toast({
       title: 'Message sent!',
       description: 'Thank you for contacting us!',
