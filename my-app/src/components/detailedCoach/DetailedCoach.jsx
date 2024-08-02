b482585575e6570e01f3202a6001dfca8428873a
Georgi Burgazliev <nanobat6@gmail.com>
2024-08-02 22:14:47 +0300

    add conditional rendering based on user role including admin functionality

--- my-app/src/components/detailedCoach/DetailedCoach.jsx
+++ my-app/src/components/detailedCoach/DetailedCoach.jsx
@@ -2,40 +2,88 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { subscribeToCoachById } from '../../utils/utils'
-  
-  const DetailedCoach = () => {
-    const { id } = useParams()
-    const [ coach, setCoach ] = useState({});
-
-    
-
-    useEffect(() => {
-        subscribeToCoachById(id.slice(1), setCoach)
-      
-    }, [id])
+import './detailedCoach.css'
+import Header from '../header/Header'
+import { isMobileDevice } from '../../utils/utils'
+import Footer from '../footer/Footer'
+import MobileHeader from '../mobileHeader/MobileHeader'
+import { useAuth } from '../../context/AuthContext'
+import { getUserById, updateCoach } from '../../utils/utils'
 
 
-useEffect(() => {   
-    console.log(coach)
-}
-, [coach])
+const DetailedCoach = () => {
+  const { id } = useParams()
+  const [coach, setCoach] = useState({});
+  const { user } = useAuth();
+  const [userData, setUser] = useState({});
+  const [coachDescription, setCoachDescription] = useState('');
+
+
+  useEffect(() => {
+    getUserById(user?.uid, setUser)
+  }, [user])
+
+
+  useEffect(() => {
+    subscribeToCoachById(id.slice(1), setCoach)
+
+  }, [id])
+
+  useEffect(() => {
+    setCoachDescription(coach?.description || '')
+
+  }, [coach])
  
-useEffect(() => {
-  console.log(id)
 
-}, [id])
-
-
-    return (
-      <div>
-        <h1>Coach</h1>
-        <h1>{coach.firstName}</h1>
-        <h1>{coach.lastName}</h1>
-        <img src={coach.imageURL} alt
-        ={`${coach.firstName} ${coach.lastName}`} />
-        
-      </div>
-    )
+  const handleSubmit = async () => {
+    await updateCoach(coach.uid, { description: coachDescription })
   }
 
-    export default DetailedCoach
+
+
+  return (
+    <> {!isMobileDevice() ? <Header /> : <div id='header'>
+      <MobileHeader />
+    </div>} <div class='detailed-coach-container'>
+
+
+        <div id="coach-details-container">
+          <img id='detailed-coach-image' src={coach.imageURL} alt
+
+            ={`${coach.firstName} ${coach.lastName}`} />
+
+          <div id='coach-description'>
+            <h1>{coach.firstName} {coach.lastName}</h1>
+            {userData?.role === 'admin' ? <><p>{coach?.description}</p>
+              <textarea id='admin-text-area' cols="30" rows="10" value={coachDescription} onChange={(e) => setCoachDescription(e.target.value)}>
+                {coach?.description}
+              </textarea>
+            </>
+                :
+              <p>{coach?.description}</p>
+            }
+
+            {userData?.role === 'admin' && coach?.description !== coachDescription && <><button onClick={() => setCoachDescription(coach?.description || '')}>Cancel</button> <button onClick={handleSubmit}>Save</button> </>}
+
+
+
+
+          </div>
+        </div>
+
+
+
+
+      </div>
+      <div id='footer'>
+        <Footer />
+      </div>
+
+    </>
+
+
+
+  )
+}
+
+export default DetailedCoach
