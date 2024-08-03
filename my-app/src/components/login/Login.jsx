6ffd6099062527f43cef89c43e096d9d8f93a9d4
Georgi Burgazliev <nanobat6@gmail.com>
2024-08-03 21:22:24 +0300

    change login and register bg color

--- my-app/src/components/login/Login.jsx
+++ my-app/src/components/login/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
   }
 
   return (
-    <Flex w='100%' minH='100vh' display='flex' direction='column' justify='space-between'  align='center' backgroundColor='black'>
+    <Flex w='100%' minH='100vh' display='flex' direction='column' justify='space-between'  align='center' backgroundColor='#1a202c'>
    
       <Flex w={['100%', '100%', '30%', '30%']} h={['60%', '60%', '40%', '40%']} marginTop={['20%', '20%', '10%', '10%']} marginBottom='10%' justify='center' align='center'>
         <Flex w={['70%', '70%', '50%', '50%']} h='50%' justify='space-evenly' direction='column'>
