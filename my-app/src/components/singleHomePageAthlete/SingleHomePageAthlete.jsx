9bd5f515c4a9ab8ee1c3d957366d6472f5d7074e
Georgi Burgazliev <nanobat6@gmail.com>
2024-08-06 17:53:41 +0300

    add css file

--- my-app/src/components/singleHomePageAthlete/SingleHomePageAthlete.jsx
+++ my-app/src/components/singleHomePageAthlete/SingleHomePageAthlete.jsx
@@ -10,6 +10,7 @@ import { Box, Text, Wrap, WrapItem, Avatar, Flex, Button } from '@chakra-ui/reac
 import { useState } from 'react'
 import { handleDeleteAthlete } from '../../utils/utils'
 import { useNavigate } from 'react-router-dom'
+import './singleHomePageAthlete.css'
 
 const SingleHomePageAthlete = ({ athlete, isAdmin }) => {
     const [isHovered, setIsHovered] = useState(false);
@@ -17,7 +18,8 @@ const SingleHomePageAthlete = ({ athlete, isAdmin }) => {
 
     return (
         <Flex w='100%' direction='column' justify='center' align='center'>
-            <Avatar name={athlete.firstname} src={athlete.picture} size={['xl', 'lg', '2xl', '2xl']} onMouseEnter={() => setIsHovered(true)}
+            <div id='athlete-pic'>
+                  <Avatar name={athlete.firstname} src={athlete.picture} size={['xl', 'lg', '2xl', '2xl']}   onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
                 position="relative">
                 {isAdmin() && isHovered && <Flex justify='space-evenly' direction='column' position='absolute'> <Button zIndex={10} colorScheme='red' onClick={() => navigate(`/detailed-athlete-view/:${athlete.uid}`)}>View profile</Button>
@@ -25,7 +27,9 @@ const SingleHomePageAthlete = ({ athlete, isAdmin }) => {
                 {!isAdmin() && isHovered && <Flex  w={['100%', '10%', '90%', '90%']} justify='center' direction='column' position='absolute'> <Button  size={['sm', 'sm', 'md', 'md']}  w='100%'  zIndex={10} colorScheme='red' onClick={() => navigate(`/detailed-athlete-view/:${athlete.uid}`)}>View profile</Button>
                 </Flex>}
             </Avatar>
-            <Flex direction='column'justify='center' align='center' w='100%'>
+            </div>
+          
+            <Flex direction='column'justify='center' align='center' w='100%' mt='5px'>
                 <Text >{athlete.firstname}</Text>
                 <Text >{ athlete.lastname}</Text>
             </Flex>
