6a567f3cd564f805c70ae0edbc9c0288203f07db
Georgi Burgazliev <nanobat6@gmail.com>
2024-08-10 17:13:11 +0300

    add lazy loading

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,30 +1,20 @@
-import { useEffect, useReducer, useState } from 'react'
-import reactLogo from './assets/react.svg'
-
-
 import './App.css'
-import { auth } from '../firebase/firebase-config'
-import { useContext } from 'react'
-import { BrowserRouter as Router, Route, Routes, Link, UNSAFE_ViewTransitionContext } from 'react-router-dom';
-import Home from './views/home/Home'
-import { useAuth } from './context/AuthContext'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header'
-import { Flex } from '@chakra-ui/react'
 import Footer from './components/footer/Footer'
-import AboutUs from './components/aboutUs/AboutUs'
-
-import { Box, Container } from '@chakra-ui/react'
-import AuthPage from './components/authPage/AuthPage'
-import Register from './components/register/Register'
-import Login from './components/login/Login'
-import DetailedAthletePage from './components/detailedAthletePage/DetailedAthletePage'
-import ContactUsPage from './components/contactUsPage/ContactUsPage'
-import Loader from './components/loader/Loader'
-import CoachesPage from './components/coachesPage/CoachesPage'
-import DetailedCoach from './components/detailedCoach/DetailedCoach'
-
 import MobileHeader from './components/mobileHeader/MobileHeader'
 import { isMobileDevice } from './utils/utils'
+import React from 'react'
+import Loader from './components/loader/Loader'
+const LazyHome = React.lazy(() => import('./views/home/Home'))
+const LazyAboutUs = React.lazy(() => import('./components/aboutUs/AboutUs'))
+const LazyRegister = React.lazy(() => import('./components/register/Register'))
+const LazyLogin = React.lazy(() => import('./components/login/Login'))
+const LazyDetailedAthletePage = React.lazy(() => import('./components/detailedAthletePage/DetailedAthletePage'))
+const LazyCoachesPage = React.lazy(() => import('./components/coachesPage/CoachesPage'))
+const LazyDetailedCoach = React.lazy(() => import('./components/detailedCoach/DetailedCoach'))
+const LazyContactUsPage = React.lazy(() => import('./components/contactUsPage/ContactUsPage'))
+
 
 function App() {
 
@@ -32,33 +22,51 @@ function App() {
   return (
 
     <div class='app'>
-       {!isMobileDevice() ? <Header /> : <MobileHeader />}
+      {!isMobileDevice() ? <Header /> : <MobileHeader />}
       <div class="main">
-      
-         <Routes>
-        <Route path='/detailed-athlete-view/:id' element={<DetailedAthletePage />} />
-        <Route path='/coaches' element={<CoachesPage />} />
-        <Route path='/contact' element={<ContactUsPage/>} />
-        <Route path='/' element={<Home />} />
-        <Route path='/about-us' element={<AboutUs />} />
-        <Route path='*' element={'Not found'} />
-        <Route path='/auth/register' element={<Register />} />
-        <Route path='/auth/login' element={<Login />} />
-        <Route path='/detailed-coach/:id' element={<DetailedCoach />} />
 
-      </Routes>
-    
-          
-      
-       
+        <Routes>
+          <Route path='/detailed-athlete-view/:id' element={< React.Suspense fallback={<Loader />} >
+            <LazyDetailedAthletePage />
+          </React.Suspense>
+          } />
+          <Route path='/coaches' element={< React.Suspense fallback={<Loader />} >
+            <LazyCoachesPage />
+          </React.Suspense>} />
+          <Route path='/contact' element={< React.Suspense fallback={<Loader />} >
+            <LazyContactUsPage />
+          </React.Suspense>} />
+
+          <Route path='/about-us' element={< React.Suspense fallback={<Loader />} >
+            <LazyAboutUs />
+          </React.Suspense>} />
+
+          <Route path='/auth/register' element={< React.Suspense fallback={<Loader />} >
+            <LazyRegister />
+          </React.Suspense>} />
+          <Route path='/auth/login' element={< React.Suspense fallback={<Loader />} >
+            <LazyLogin />
+          </React.Suspense>} />
+          <Route path='/detailed-coach/:id' element={< React.Suspense fallback={<Loader />} >
+            <LazyDetailedCoach />
+          </React.Suspense>} />
+          <Route path='/' element={< React.Suspense fallback={<Loader />} >
+            <LazyHome />
+          </React.Suspense>} /> <Route path='*' element={'Not found'} />
+        </Routes>
+
+
+
+
+
       </div>
 
       <Footer />
 
     </div>
 
-      
-    
+
+
 
 
 
--- my-app/src/components/loader/Loader.jsx
+++ my-app/src/components/loader/Loader.jsx
@@ -1,15 +1,16 @@
 import { Spinner , Flex} from '@chakra-ui/react'
-
+import './loader.css'
+import loading from '../../assets/loading.png'
 const Loader = () => {
     return (
         <div id='loader'>
-   <Spinner
-        thickness="4px"
-        speed="0.65s"
-        emptyColor="gray.200"
-        color="blue.500"
-        size="xl"
-        />  
+           <div id="cloud-container">
+             <img src={loading} class='spin' alt="cloud" />
+            <img src={loading}  class='spinDelayHalfSec' alt="cloud" />
+            <img src={loading} class='spinDelay1sec'alt="cloud" />
+           </div>
+           
+
 
         </div>
          
