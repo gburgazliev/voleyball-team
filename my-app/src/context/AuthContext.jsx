30eef48fa8afa78de8512b6e012259b75f44c101
gburgazliev <burgazlievgeorgi@gmail.com>
2024-11-02 16:56:20 +0200

    add setPersistance and browserLocalPersistance for longer sessions, add useEffect which uses fetchUserData function to get the current user's data

--- my-app/src/context/AuthContext.jsx
+++ my-app/src/context/AuthContext.jsx
@@ -1,46 +1,66 @@
-
 import { auth } from "../../firebase/firebase-config";
-import {useAuthState} from "react-firebase-hooks/auth";
-import { createContext } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { createContext, useState, useEffect } from "react";
 import { useContext } from "react";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import PropTypes from "prop-types";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  browserLocalPersistence,
+  setPersistence,
+} from "firebase/auth";
+import { fetchUserData } from "../utils/utils";
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-     const [user, loading, error] = useAuthState(auth);
-    
+  const [user, loading, error] = useAuthState(auth);
+  const [userData, setUserData] = useState(null);
 
-
-        // Define authentication functions
-  const register = (email, password) => {
+  // Define authentication functions
+  const register = async (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  const login = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
+  const login = async (email, password) => {
+    await setPersistence(auth, browserLocalPersistence);
+    await signInWithEmailAndPassword(auth, email, password);
   };
 
-  const logout = () => {
-    return signOut(auth);
+  const logout = async () => {
+    await signOut(auth);
   };
 
-       
+  useEffect(() => {
+    if (user) {
+      fetchUserData(user.uid, setUserData);
+    } else {
+      setUserData(null);
+    }
+  }, [user]);
 
-     const valueData = {
-        user, loading, error, register, login, logout
-     };
+  const valueData = {
+    userData,
+    loading,
+    error,
+    register,
+    login,
+    logout,
+  };
 
+  return (
+    <AuthContext.Provider value={valueData}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
 
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 
-    
-    return (
-        <AuthContext.Provider value={valueData}>
-        {!loading && children}
-        </AuthContext.Provider>
-    );
-    };
-
-    export const useAuth = () => {
-        return useContext(AuthContext);
-    };
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
