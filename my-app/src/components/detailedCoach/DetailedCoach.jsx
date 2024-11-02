f8aa13c911d5bdf6c48ac42b05b052735b3217f9
gburgazliev <burgazlievgeorgi@gmail.com>
2024-11-02 17:01:13 +0200

    remove redunant code and format

--- my-app/src/components/detailedCoach/DetailedCoach.jsx
+++ my-app/src/components/detailedCoach/DetailedCoach.jsx
@@ -1,88 +1,82 @@
-
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { subscribeToCoachById } from '../../utils/utils'
-import './detailedCoach.css'
-import { useAuth } from '../../context/AuthContext'
-import { getUserById, updateCoach } from '../../utils/utils'
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { subscribeToCoachById } from "../../utils/utils";
+import "./detailedCoach.css";
+import { useAuth } from "../../context/AuthContext";
+import { updateCoach } from "../../utils/utils";
+import { Box } from "@chakra-ui/react";
 
 const DetailedCoach = () => {
-  const { id } = useParams()
+  const { id } = useParams();
   const [coach, setCoach] = useState({});
-  const { user } = useAuth();
-  const [userData, setUser] = useState({});
-  const [coachDescription, setCoachDescription] = useState('');
-  const formatCoachDescription = coach?.description ? coach?.description.split('\n').map((string, index) => {
-    return <li key={index}>{string}</li>
-  })
+  const { userData } = useAuth();
+
+  const [coachDescription, setCoachDescription] = useState("");
+  const formatCoachDescription =  coach?.description
+    ? coach?.description.split("\n").map((string, index) => {
+        return <li key={index}>{string}</li>;
+      })
     : [];
 
-
-
-  useEffect(() => {
-    getUserById(user?.uid, setUser)
-  }, [user])
-
-
-  useEffect(() => {
-    subscribeToCoachById(id.slice(1), setCoach)
-
-  }, [id])
-
-  useEffect(() => {
-    setCoachDescription(coach?.description || '')
-
-  }, [coach])
-
-
   const handleSubmit = async () => {
-    await updateCoach(coach.uid, { description: coachDescription })
-  }
+    await updateCoach(coach.uid, { description: coachDescription });
+  };
+  useEffect(() => {
+    const unsubscribe = subscribeToCoachById(id.slice(1), setCoach);
 
+    return () => unsubscribe();
+  }, [id]);
 
+  useEffect(() => {
+    setCoachDescription(coach?.description || "");
+  }, [coach]);
 
   return (
-    <div className='detailed-coach-container'>
-
-
+    <Box w={['90%' , '80%', '80%', '50%']} paddingTop={['30px', '30px' ,'30px' , '0px']}>
       <div id="coach-details-container">
-        <img id='detailed-coach-image' src={coach.imageURL} alt
+        <img
+          id="detailed-coach-image"
+          src={coach.imageURL}
+          alt={`${coach.firstName} ${coach.lastName}`}
+        />
 
-          ={`${coach.firstName} ${coach.lastName}`} />
-
-        <div id='coach-description'>
-          <h1>{coach.firstName} {coach.lastName}</h1>
-          {userData?.role === 'admin' ? <><p>{coach?.description}</p>
-            <textarea id='admin-text-area' cols="30" rows="10" value={coachDescription} onChange={(e) => setCoachDescription(e.target.value)}>
-              {coach?.description}
-            </textarea>
-          </>
-            :
-            <or>
+        <div id="coach-description">
+          <h1>
+            {coach.firstName} {coach.lastName}
+          </h1>
+          {userData?.role === "admin" ? (
+            <>
               {formatCoachDescription}
-              </or>
-          }
+              <textarea
+                id="admin-text-area"
+                cols="30"
+                rows="10"
+                value={coachDescription}
+                onChange={(e) => setCoachDescription(e.target.value)}
+              >
+                
+              
+              </textarea>
+            </>
+          ) : (
+            <or>{formatCoachDescription}</or>
+          )}
 
-          {userData?.role === 'admin' && coach?.description !== coachDescription && <><button onClick={() => setCoachDescription(coach?.description || '')}>Cancel</button> <button onClick={handleSubmit}>Save</button> </>}
-
-
-
-
+          {userData?.role === "admin" &&
+            coach?.description !== coachDescription && (
+              <>
+                <button
+                  onClick={() => setCoachDescription(coach?.description || "")}
+                >
+                  Cancel
+                </button>
+                <button onClick={handleSubmit}>Save</button>{" "}
+              </>
+            )}
         </div>
       </div>
+    </Box>
+  );
+};
 
-
-
-
-    </div>
-
-
-
-
-
-
-  )
-}
-
-export default DetailedCoach
+export default DetailedCoach;
