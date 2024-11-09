51058313a6ff4d71959462d7bd120c3dd22f6ab8
gburgazliev <burgazlievgeorgi@gmail.com>
2024-11-09 15:17:02 +0200

    change from flex to grid

--- my-app/src/views/home/Home.jsx
+++ my-app/src/views/home/Home.jsx
@@ -3,7 +3,7 @@
  * @returns {JSX.Element} The rendered home page component.
  */
 import {
-    Flex
+    Flex, Grid, GridItem, Image
 } from "@chakra-ui/react"
 import logo from '../../assets/logo.jpg'
 import banner2 from '../../assets/banner2.jpg'
@@ -21,18 +21,21 @@ const Home = () => {
 
         <Flex direction='column' justify='center'  align='center' w={['100%', '100%', '100%', '100%']} >
             
-        
+{/*         
              <div class='banner-container'>
                  <img id='banner' src={banner2}  alt='Volleyball Team' />
              </div>
-               
+                */}
 
             
 
-            <Flex justify='center' padding={2} gap='5%' align='center' w={['100%', '100%', '100%', '100%']}>
-                <img id='logo' src={logo}  alt='Volleyball Team' />
+            <Grid  padding={5}  gap={['0%', '0%' , '5%' ,'5%']} align='center' w='100%' templateColumns={['1fr' , '1fr', '1fr 1fr', '1fr 1fr']}>
+                <GridItem display='flex' justifyContent={['center', 'center', 'end' ,'end']} >
+                     <Image width={['90%', '70%', '90%', '70%']} height='auto' src={logo}  alt='Volleyball Team' objectFit='contain' />
+                </GridItem>
+               
                 
-                
+                <GridItem display="flex" justifyContent={["center" , 'center' , 'start' ,'start']} alignItems="center">
                       <ul  
                     >
                         <li>Team searching</li>
@@ -42,12 +45,14 @@ const Home = () => {
                         <li>Physical Conditioning</li>
                         <li>Education, individual trainings, sports management, volleyball</li>
                     </ul>
+                </GridItem>
+                    
                 
                   
 
              
 
-            </Flex>
+            </Grid>
 
             <HomePageAthletes />
 
