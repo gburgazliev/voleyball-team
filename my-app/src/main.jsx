db381ccd496764440d1d3872f82bae837c82660c
gburgazliev <burgazlievgeorgi@gmail.com>
2024-11-09 15:05:21 +0200

    change chakra provider

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -4,15 +4,18 @@ import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import {ChakraProvider} from '@chakra-ui/react'
+import {ChakraProvider } from '@chakra-ui/react'
+import { Provider } from "./components/ui/provider"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <AuthProvider>
-      <ChakraProvider>
-      <App />
-      </ChakraProvider>
+      <Provider>
+<App />
+      </Provider>
+      
+    
     </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
