d430e0bff3913cb729a39afab46fadc5e47dbcb7
gburgazliev <burgazlievgeorgi@gmail.com>
2024-11-19 14:10:59 +0200

    add motion components

--- my-app/src/components/motionComponents/motionComponents.jsx
+++ my-app/src/components/motionComponents/motionComponents.jsx
@@ -1,6 +1,10 @@
-import { Button, GridItem, Text } from "@chakra-ui/react";
+import { Button, GridItem, Text, List,Flex  } from "@chakra-ui/react";
 import {motion} from 'framer-motion'
 
 export const MotionButton = motion(Button);
 export const MotionText = motion(Text);
 export const MotionGridItem = motion(GridItem);
+export const MotionList = motion(List.Root);
+export const MotionListItem = motion(List.Item);
+
+export const MotionFlex = motion(Flex);
