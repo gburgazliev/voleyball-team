302ba24ca974d340a3a82d74bdc12be0187b91e2
gburgazliev <burgazlievgeorgi@gmail.com>
2024-11-24 00:41:10 +0200

    format and add isAdmin prop instead of user

--- my-app/src/components/singleCoachesPageCoach/SingleCoachesPageCoach.jsx
+++ my-app/src/components/singleCoachesPageCoach/SingleCoachesPageCoach.jsx
@@ -1,43 +1,35 @@
-import './singleCoach.css'
-import {  deleteStorageObject, deleteDatabaseObject } from '../../utils/utils'
+import "./singleCoach.css";
+import { deleteStorageObject, deleteDatabaseObject } from "../../utils/utils";
 
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
-const SingleCoachesPageCoach = ({user, coach}) => {
+const SingleCoachesPageCoach = ({ isAdmin, coach }) => {
   const navigate = useNavigate();
-      
 
   const handleDeleteCoach = async () => {
-    if (coach.imageURL){
+    if (coach.imageURL) {
       await deleteStorageObject(coach.imageURL);
     }
-        await deleteDatabaseObject(`coaches/${coach.uid}`);
-  }
+    await deleteDatabaseObject(`coaches/${coach.uid}`);
+  };
 
+  return (
+    <div className="single-coach-container">
+      <img className="coach-image" src={coach.imageURL} alt="" />
 
-
-     
-  
-    return (
-        <div class='single-coach-container'>
-          
-                <img class='coach-image' src={coach.imageURL} alt="" />
-             
-              
-              
-                 <div className="navigation">
-                  <div id='coach-names'> 
-                    <p class='single-coach-name'>{coach.firstName} </p> 
-                    <p class='single-coach-name'>{coach.lastName}</p>
-                  </div>
-               
-                <button onClick={() => navigate(`/detailed-coach/:${coach.uid}`)}>View profile</button>
-                {user && user.role === 'admin' ? <button onClick={handleDeleteCoach}>Delete</button> : null}
-               
-               </div>
-                
+      <div className="navigation">
+        <div id="coach-names">
+          <p className="single-coach-name">{coach.firstName} </p>
+          <p className="single-coach-name">{coach.lastName}</p>
         </div>
-    )
-}
 
-export default SingleCoachesPageCoach
+        <button  onClick={() => navigate(`/detailed-coach/:${coach.uid}`)}>
+          View profile
+        </button>
+        {isAdmin ? <button onClick={handleDeleteCoach}>Delete</button> : null}
+      </div>
+    </div>
+  );
+};
+
+export default SingleCoachesPageCoach;
